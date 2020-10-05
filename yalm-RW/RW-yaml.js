4c8993cf7ea3680595169a6e01e958d815976d5b
chan9948 <chan9948@gmail.com>
2020-10-05 14:10:54 +0800

    yaml read write test

---
+++ yalm-RW/RW-yaml.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+try {
+    let fileContents = fs.readFileSync('./test.yaml', 'utf8');
+    let data = yaml.safeLoad(fileContents);
+
+    console.log(data);
+
+    fs.writeFile('./test-output.yaml', yaml.safeDump(data), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+} catch (e) {
+    console.log(e);
+}
