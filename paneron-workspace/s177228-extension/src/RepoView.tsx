62b9d2762165889ce78246ad755c94218e663676
chan9948 <chan9948@gmail.com>
2021-01-21 15:25:03 +0800

    test 4

--- paneron-workspace/s177228-extension/src/RepoView.tsx
+++ paneron-workspace/s177228-extension/src/RepoView.tsx
@@ -2,19 +2,35 @@ import { remote } from 'electron';
 import React, { useState, useEffect } from 'react';
 import { RepositoryViewProps } from '@riboseinc/paneron-extension-kit/types';
 
-import {AddressProfileView} from "./AddressProfileView";
-
 export const RepositoryView: React.FC<RepositoryViewProps> =
 function (props) {
 
   const [busy,setBusy] = useState(false);
+
+  class AddressProfileView extends React.Component {
+    render() {
+        return(
+            <>
+                <div>Profile Setting</div>
+                <div>
+                    <label htmlFor="addressProfile">Address Profile Country:</label>
+                    <select name="addressProfile" id="addressProfile">
+                        <option value="HKG">HKG</option>
+                        <option value="US">US</option>
+                        <option value="TW">TW</option>
+                    </select>
+                </div>
+            </>
+        );
+    }
+}
 
   return(
     <div>
       {busy
       ? "loading"
       : <>
-        <div>test3</div>
+          <AddressProfileView/>
         </>
       }
     </div>
