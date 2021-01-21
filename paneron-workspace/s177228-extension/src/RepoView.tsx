21e934973fffabfeeb5c52d2610595f4a1c13d4e
chan9948 <chan9948@gmail.com>
2021-01-21 16:39:25 +0800

    rebase init

--- paneron-workspace/s177228-extension/src/RepoView.tsx
+++ paneron-workspace/s177228-extension/src/RepoView.tsx
@@ -7,49 +7,14 @@ import iso3166code from './data/iso3166code.json';
 export const RepositoryView: React.FC<RepositoryViewProps> =
 function (props) {
 
-  const [busy,setBusy] = useState(false);
-
-
-  function AddressProfileOptions(props) {
-    const list = {
-      {name: "HONG KONG", code: "HKG"},
-      {name: "Taiwan", code: "TW"},
-    };
-    const options = list.map((item)=>{
-      <>
-        <option value={item.code}>
-          <span>{item.name}</span>
-          <span>{item.code}</span>
-        </option>
-      </>
-    });
-    return(
-      <>
-        <label htmlFor="addressProfile">Address Profile Country:</label>
-        <select name="addressProfile" id="addressProfile">
-          {options}
-        </select>
-      </>
-    );
-  }
-
-  function AddressProfileView(props) {
-    return(
-      <>
-        <div>Profile Setting</div>
-        <div>
-          <AddressProfileOptions/>
-        </div>
-      </>
-    );
-  }
+  const [busy,setBusy] = useState(false)
 
   return(
     <div>
       {busy
         ? "Loading"
         : <>
-            <AddressProfileView/>
+            <div>init</div>
           </>
       }
     </div>
