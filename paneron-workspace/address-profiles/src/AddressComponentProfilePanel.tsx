e736319ddb218c52efeae2f343b743e59128a10c
s177228 <s177228@hsu.edu.hk>
2021-02-11 15:50:49 +0800

    updated list

--- paneron-workspace/address-profiles/src/AddressComponentProfilePanel.tsx
+++ paneron-workspace/address-profiles/src/AddressComponentProfilePanel.tsx
@@ -1,6 +1,7 @@
-import { AnchorButton, InputGroup, NumericInput, TagInput } from "@blueprintjs/core";
+import { AnchorButton, Collapse, InputGroup, NumericInput, TagInput } from "@blueprintjs/core";
 import * as React from "react";
 import { AddressComponentProfile, AddressProfile, AttributeProfile } from "./AddressProfile";
+import { AttributeProfilePanel } from "./AttributeProfilePanel";
 
 export class AddressComponentProfilePanel extends React.Component<AddressComponentProfilePanelProps> {
     constructor(props){
@@ -181,6 +182,7 @@ class AddressComponentProfileListItem extends React.Component<AddressComponentPr
         super(props);
         this.state = {
             // Form state
+            isListOpen: false,
             isEditingForm: false,
 
             // Profile Data
@@ -197,6 +199,10 @@ class AddressComponentProfileListItem extends React.Component<AddressComponentPr
 
             combinedAttributeProfiles: [],
         }
+    }
+
+    handleListOpen = () => {
+        this.setState({isListOpen: !this.state.isListOpen});
     }
 
     handleEditProfile = () => {
@@ -293,8 +299,43 @@ class AddressComponentProfileListItem extends React.Component<AddressComponentPr
         } as React.CSSProperties;
 
         const subItemSytle = {
-            backgroundColor: "#363",
-            color: "#FFF",
+            backgroundColor: "#99BB99",
+            // color: "#FFF",
+        }
+
+        const subItemHeadSytle ={
+            padding: "0.5em 0 1.5em 0",
+            fontSize: "1.1em",
+            textAlign: "center"
+        }
+
+        const centerStyle = {
+            fontSize: "1.2em",
+            textAlign: "center",
+            cursor: "pointer",
+            paddingBottom: "1em",
+            lineHeight: "0",
+        }
+
+        const rightDivStyle = {
+            float: "right",
+        }
+
+        const subSubItemSytle = {
+            backgroundColor: "#779977",
+            marginLeft: "5px",
+            marginRight: "5px", 
+            // color: "#FFF",
+        }
+
+        const subSubItemHeadStyle = {
+            padding: "7px 5px 5px",
+            fontSize: "15px",
+            height: "unset",
+        }
+
+        const subSubitemBodyStyle = {
+            fontSize: "15px",
         }
 
         return(
@@ -343,20 +384,45 @@ class AddressComponentProfileListItem extends React.Component<AddressComponentPr
                         </tr>
                     </table>
                     <div style={{...itemStyle, ...subItemSytle}}>
-                        <div style={itemHeadStyle}>Attribute Profile</div>
+                        <div style={{...itemHeadStyle, ...subItemHeadSytle}}>Attribute Profile X{this.state.combinedAttributeProfiles.length}</div>
                         <hr style={itemHrStyle} />
-                        <div style={itemBodyStyle}>
+                        <Collapse isOpen={this.state.isListOpen} style={itemBodyStyle}>
                                 {
                                     this.state.combinedAttributeProfiles.map((combinedAttribute)=>(
-                                        <div key={combinedAttribute.name}>
-                                            <div>{combinedAttribute.name}</div> 
+                                        <div style={{...itemStyle, ...subSubItemSytle}} key={combinedAttribute.name}>
+                                            <div style={{...itemHeadStyle, ...subSubItemHeadStyle}}>
+                                                {combinedAttribute.name}
+                                                <div style={rightDivStyle}>
+                                                        min: {combinedAttribute.minCardinality} | max: {combinedAttribute.maxCardinality}
+                                                </div>
+                                                <div style={rightDivStyle}>
+                                                {
+                                                            this.state.isEditingForm
+                                                            ?<AnchorButton onClick={(event)=>{
+                                                                //todo
+                                                            }} intent="danger" icon="delete" text="Remove Inculded Attribute" style={{marginLeft: "5px"}}/>
+                                                            :<></>
+                                                        }
+                                                </div>
+                                            </div>
+                                            <hr style={itemHrStyle} />
+                                            <div style={{...itemBodyStyle,...subSubitemBodyStyle}}>
+                                                <table>
+                                                    <tr>
+                                                        <td>Value Type</td><td>:</td><td>{combinedAttribute.valueType}</td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                            
+                                            {/* <div>{combinedAttribute.name}</div> 
                                             <div>{combinedAttribute.maxCardinality}</div> 
                                             <div>{combinedAttribute.minCardinality}</div>  
-                                            <div>{combinedAttribute.valueType}</div>  
+                                            <div>{combinedAttribute.valueType}</div>   */}
                                         </div>
                                     ))
                                 }
-                        </div>
+                        </Collapse>
+                        <div style={centerStyle} onClick={this.handleListOpen}>...</div>
                     </div>
                 </div>
             </div>
